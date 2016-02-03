984dc3ff19efbfd5c85f1727e65f764d8746e222
twaddlac <twaddlac@gmail.com>
2016-02-03 11:30:59 -0500

    Update run-tophat.sh

--- run-tophat.sh
+++ run-tophat.sh
@@ -9,6 +9,10 @@ module load tophat/intel/2.0.12
 module load bowtie2
 #module switch samtools/intel/0.1.19 samtools/intel/1.2
 
-cd /scratch/at120/shared/laura-alan/2015-10-01_C68Y0ACXX-redo/staph-tophat
+#change path to where the fastq files are
+cd $path
+# tophat command was taken from /scratch/at120/shared/alex/2015-07-24_H55MKBCXX/Replicate_Data/am-new-tophat-out/accepted_hits.bam
+# /scratch/at120/shared/alex/2015-07-24_H55MKBCXX/Replicate_Data/brugia_wolbachia_v4.gff3
 
-tophat2 -o $fastq-tophat -p 12 --microexon-search --b2-very-sensitive -G staph-aureus-nctc-8325/staph-nctc-8325.gff3 staph-aureus-nctc-8325/staph-nctc-8325.fa $fastq.r1.fastq.gz $fastq.r2.fastq.gz
+tophat2 --b2-very-sensitive -o $fastq-tophat-out -p 12 -r 50 --mate-std-dev 100 -G /scratch/at120/shared/db/organism/brugia/b_malayi.PRJNA10729.WS252.annotations.gff3 /scratch/at120/shared/db/organism/brugia/b_malayi.PRJNA10729.WS252.genomic_masked.fa $fastq.r1.fastq $fastq.r2.fastq
+
